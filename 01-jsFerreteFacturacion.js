22968b17ff817e3fffeddf3a51b9ddecdf2ef699
PabloEzequielCampos <68036545+PabloEzequielCampos@users.noreply.github.com>
2020-07-23 22:44:21 -0300

    Add files via upload

---
+++ 01-jsFerreteFacturacion.js
@@ -0,0 +1,58 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+function Sumar () 
+{
+
+let precio1;
+let precio2;
+let precio3;
+let Resultado; 
+    //Les asigno un espacio para ingresar el valor y que se guarde en la variable
+    precio1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+        // Resuelvo
+        Resultado = precio1 + precio2 + precio3;
+                //Cartel emergente con mensaje deseado
+                 alert("La suma de los pruductos es : $"  +Resultado.toFixed(2));
+}
+
+function Promedio () 
+{
+
+let precio1;
+let precio2;
+let precio3;
+let resultado;
+let promedio;
+    //Les asigno un espacio para ingresar el valor y que se guarde en la variable
+    precio1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    //Resuelvo y promedio
+    resultado = precio1+ precio2 + precio3;
+    promedio  = resultado / 3;
+                //Cartel emergente con mensaje deseado
+                alert("El promedio generale es : $" +promedio.toFixed(2));
+}
+
+function PrecioFinal () 
+{
+//asigno variables
+let precio1;
+let precio2;
+let precio3;
+let preciofinal;
+    //Les asigno un espacio para ingresar el valor y que se guarde en la variable
+    precio1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    //Resuelvo y sumo el 21% de iva
+    preciofinal = (precio1 + precio2 + precio3) *1.21;
+                //Cartel emergente con mensaje deseado 
+                alert("el precio final con el 21% de iva es : $" +preciofinal.toFixed(2));
+
+}
---
+++ 03-jsFerretePinturas.js
@@ -0,0 +1,35 @@
+/*3.	Para el departamento de Pinturas:
+A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+*/
+function FahrenheitCentigrados () 
+{
+    let vGradosFahrenheit;
+    let vConvercion;
+
+    vGradosFahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    vConvercion = ((vGradosFahrenheit - 32)*5/9).toFixed(2);
+
+    alert(+vGradosFahrenheit+"  Fahrenheit son : " +vConvercion+"  Grados centígrados. ");
+   // alert(`${GradosFahrenheit} Fahrenheit son ${Convercion} Grados centígrados`);
+}
+
+
+function CentigradosFahrenheit () 
+{
+    let vGradosCentigrados;
+    let vConvercion;
+
+    vGradosCentigrados = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+
+// Se invierte la ecuacion -32 *5/9  a    *9/5 + 32.  
+
+    vConvercion = ((vGradosCentigrados * 9/5)+32).toFixed(2);
+
+    alert(vGradosCentigrados+" Centigrados : "+vConvercion+" Fahrenheit. ");
+
+    //Otra forma de aplicarlo
+    //alert(`${GradosCentigrados} Fahrenheit son ${Convercion} Grados centígrados`);
+}
